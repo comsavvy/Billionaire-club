67c3771f0490b73e4fbec6f0ca901327f196a086
comsavvy <olusolaogundepo@gmail.com>
2020-08-10 17:22:59 +0100

    Main function added to the program

--- lesson.py
+++ lesson.py
@@ -1,44 +1,45 @@
 def am(age, money, output_name):
-    if ((age >= 20 and money >= 500000) or (age <= 20 and money >= 2000000)) is True:
+    if ((age >= 20 and money >= 500000) or (age <= 20 and money >= 2000000)):
         return True
     else:
         return False
 
 
-def male():
+def male(age, money, output_name):
     if am(age, money, output_name):
         print(f'Mr {output_name}\b, you are allow to enter😎')
     else:
         print(f'Mr {output_name}\b, please go and hustle.')
 
 
-def female():
+def female(age, money, output_name):
     if am(age, money, output_name) is True:
         print(f'Mrs {output_name}\b, you are allow to enter😎')
     else:
         print(f'Mrs {output_name}\b, please go and hustle.')
 
 
-# Question
+def main():
+	print('\t \tBILLIONAIRE CLUB')
+	try:
+	    name= input('Name: ').split()
+	    capitalize_name= [cpa.capitalize() for cpa in name]
+	    output_name= ''
+	    for cpl in capitalize_name:
+	        output_name += cpl + ' '
+	    age= int(input('Please Enter your Age: '))
+	    money= int(input('How much do you have in your account? '))
+	    gender = input('Male(M) or Female(F): ').capitalize()
+	# Conditional Statement
+	    if gender.startswith('M'):
+	        male(age, money, output_name)
+	    elif gender.startswith('F'):
+	        female(age, money, output_name)
+	    else:
+	        print('Invalid gender')
+	except ValueError:
+	    print('Check the input!')
+
+
 if __name__ == '__main__':
-    print('\t \tBILLIONAIRE CLUB')
-    try:
-        name= input('Name: ').split()
-        capitalize_name= [cpa.capitalize() for cpa in name]
-        output_name= ''
-        for cpl in capitalize_name:
-            output_name += cpl + ' '
-
-        age= int(input('Please Enter your Age: '))
-        money= int(input('How much do you have in your account? '))
-        gender = input('Male(M) or Female(F): ').capitalize()
-
-    # Conditional Statement
-        if gender == 'M' or gender== 'Male':
-            male()
-        elif gender == 'F' or gender == 'Female':
-            female()
-        else:
-            print('Invalid gender')
-    except ValueError:
-        print('Check the input!')
+	main()
